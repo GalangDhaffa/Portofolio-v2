1ea720814ec19dca84e89a1172e0f048309746fa
GalangDhaffa <davagalang3456@gmail.com>
2024-07-30 14:06:19 +0700

    first commit

---
+++ public/include/footer.php
@@ -0,0 +1,18 @@
+<div>
+  <hr>
+  <footer class="d-flex flex-wrap justify-content-between align-items-center border-top">
+    <div class="col-md-4 d-flex align-items-center mx-4">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+      <img class="bi me-2 ps-3" width="40" height="40" role="img" aria-label="Bootstrap" src="./img/logo.jpg" alt="logo">
+      </a>
+      <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 pe-3 justify-content-end list-unstyled d-flex"> 
+      <li class="ms-3"><a class="text-body-secondary fs-4" href="https://github.com/GalangDhaffa"><i class="bi bi-github"></i></a></li>
+      <li class="ms-3"><a class="text-body-secondary fs-4" href="https://www.instagram.com/galangdhaffa"><i class="bi bi-instagram"></i></a></li>
+      <li class="ms-3"><a class="text-body-secondary fs-4" href="#"><i class="bi bi-youtube"></i></a></li>
+      <li class="ms-3 me-4"><a class="text-body-secondary fs-4" href="#"><i class="bi bi-tiktok"></i></a></li>
+    </ul>
+  </footer>
+</div>
---
+++ public/include/header.php
@@ -0,0 +1,57 @@
+<header>
+  <div class="px-3 py-2 bg-zinc-800 border-bottom">
+    <div class="px-8">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="#" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+          <img class="bi me-2" width="50" height="50" role="img" aria-label="Langz7z" src="./img/logo.jpg" alt="logo">
+        </a>
+
+        <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+          <li>
+            <a href="#home" class="nav-link text-white">
+              <svg class="bi d-block mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+                <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
+              </svg>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#aboutme" class="nav-link text-white">
+              <svg class="bi d-block mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+              </svg>
+              About Me
+            </a>
+          </li>
+          <li>
+            <a href="#service" class="nav-link text-white">
+              <svg class="bi d-block mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M16 4.5a4.5 4.5 0 0 1-1.703 3.526L13 5l2.959-1.11q.04.3.041.61" />
+                <path d="M11.5 9c.653 0 1.273-.139 1.833-.39L12 5.5 11 3l3.826-1.53A4.5 4.5 0 0 0 7.29 6.092l-6.116 5.096a2.583 2.583 0 1 0 3.638 3.638L9.908 8.71A4.5 4.5 0 0 0 11.5 9m-1.292-4.361-.596.893.809-.27a.25.25 0 0 1 .287.377l-.596.893.809-.27.158.475-1.5.5a.25.25 0 0 1-.287-.376l.596-.893-.809.27a.25.25 0 0 1-.287-.377l.596-.893-.809.27-.158-.475 1.5-.5a.25.25 0 0 1 .287.376M3 14a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
+              </svg>
+              Service
+            </a>
+          </li>
+          <li>
+            <a href="#project" class="nav-link text-white">
+              <svg class="bi d-block mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1M.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8z" />
+              </svg>
+              Project
+            </a>
+          </li>
+          <li>
+            <a href="#contact" class="nav-link text-white">
+              <svg class="bi d-block mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+              </svg>
+              Contact
+            </a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
